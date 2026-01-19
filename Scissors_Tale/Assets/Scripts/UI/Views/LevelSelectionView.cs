e9b41a3da56c38f734444816588bb2c58b9ab7a7
B1ue-Whale <graywolf0313@gmail.com>
2026-01-19 20:03:18 +0900

    [Fix]메뉴 브금,스테이지선택 브금

--- Scissors_Tale/Assets/Scripts/UI/Views/LevelSelectionView.cs
+++ Scissors_Tale/Assets/Scripts/UI/Views/LevelSelectionView.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement;
 public class LevelSelectionView : Singleton<LevelSelectionView>
 {
     public void OnStageZeroButtonClicked() {
+        SoundManager.Instance.PlaySFX("Click");
         SceneManager.LoadScene("Synopsis");
     }
 
     public void OnStageOneButtonClicked() {
+        SoundManager.Instance.PlaySFX("Click");
         SceneManager.LoadScene("Testscene");
     }
     
--- Scissors_Tale/Assets/Scripts/UI/Views/MainView.cs
+++ Scissors_Tale/Assets/Scripts/UI/Views/MainView.cs
@@ -3,11 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainView : Singleton<MainView>
 {
+    private void Start()
+    {
+        SoundManager.Instance.PlayBGM("Menu_BGM");
+    }
     public void OnGameStartClicked() {
+        SoundManager.Instance.PlaySFX("Click");
         SceneManager.LoadScene("StageSelection");
     }
 
     public void OnEndGameClicked() {
+        SoundManager.Instance.PlaySFX("Click");
         UnityEditor.EditorApplication.isPlaying=false;  
         
         Application.Quit();
