39e75d3f8e06d8c35ba7b6c38a42b8a2ba6d5508
denny033089 <denny033089@gmail.com>
2026-01-19 17:07:23 +0900

    [Add] mainmenuscene worldselectionscene

--- Scissors_Tale/Assets/Scripts/UI/Views/LevelSelectionView.cs
+++ Scissors_Tale/Assets/Scripts/UI/Views/LevelSelectionView.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelSelectionView
+public class LevelSelectionView : Singleton<LevelSelectionView>
 {
+    public void OnStageZeroButtonClicked() {
+        SceneManager.LoadScene("Synopsis");
+    }
+
+    public void OnStageOneButtonClicked() {
+        SceneManager.LoadScene("Testscene");
+    }
     
 }
