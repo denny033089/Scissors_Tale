ce659aef0b0837c3afa0b43cdbc727501f01cb6a
denny033089 <denny033089@gmail.com>
2026-01-21 20:55:31 +0900

    [Fix] 빌드용으로 오류 수정

--- Scissors_Tale/Assets/Scripts/UI/Views/MainView.cs
+++ Scissors_Tale/Assets/Scripts/UI/Views/MainView.cs
@@ -14,11 +14,15 @@ public class MainView : MonoBehaviour
 
     public void OnEndGameClicked() {
         SoundManager.Instance.PlaySFX("Click");
-        UnityEditor.EditorApplication.isPlaying=false;  
-        
-        GameSystemManager.Instance.ChangeGameState(Enums.GameState.Quit);
-        
 
+        #if UNITY_EDITOR
+        // 유니티 에디터에서 실행 중일 때만 작동  01.21 정수민
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        // 실제 빌드된 게임(PC, 모바일 등)에서 실행될 때 작동
+        Application.Quit();
+        #endif
+        
     }
     
 }
