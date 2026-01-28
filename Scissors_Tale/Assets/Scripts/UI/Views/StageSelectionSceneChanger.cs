0515d9c044e70b3da8d019204d4b96b9fa96ae71
denny033089 <denny033089@gmail.com>
2026-01-28 20:27:34 +0900

    [Fix] 씬 매니저 구조 수정, stagebutton 삭제

--- Scissors_Tale/Assets/Scripts/UI/Views/StageSelectionSceneChanger.cs
+++ Scissors_Tale/Assets/Scripts/UI/Views/StageSelectionSceneChanger.cs
@@ -12,18 +12,27 @@ public class StageSelectionSceneChanger : BaseSceneChanger
 
     }
     
-    public void OnStageClicked(int Stagenum) {
+    public void OnStageClicked(int Stageindex) {
         SoundManager.Instance.PlaySFX("Click");
 
-        if(Stagenum == 1) {
+        if(Stageindex == 1) {
             GameSystemManager.Instance.ChangeGameState(Enums.GameState.Tutorial);
-            ChangeScene(nextScenes[Stagenum]);
+            ChangeScene(nextScenes[Stageindex]);
 
         } else {
             GameSystemManager.Instance.ChangeGameState(Enums.GameState.InGame);
-            ChangeScene(nextScenes[Stagenum]);
+            ChangeScene(nextScenes[Stageindex]);
         }
 
+    }
+
+    // 1. 스테이지 선택 씬에서 사용할 때: 버튼마다 인덱스 번호를 지정 (0, 1, 2...)
+    public void OnStageNumClick(int Stagenum)
+    {
+        StageDataManager.Instance.currentStageIndex = Stagenum;
+
+        // 선택한 번호의 데이터를 정적 변수에 저장
+        GameManager.SelectedMapData = StageDataManager.Instance.allStages[Stagenum];
     }
     
 }
